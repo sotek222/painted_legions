ff9d263271d7676ce79bb54c6cfaf57e901a13cd
Matthew Masiello <sotek222@yahoo.com>
2020-04-06 01:21:07 -0400

    adds palette array to send to views

--- routes/guideRoutes.js
+++ routes/guideRoutes.js
@@ -1,10 +1,15 @@
 const Router = require('express').Router();
+const { ostermark } = require('../util/palletes');
+
+Router.get('/', (req, resp, _) => {
+  resp.render('guides');
+});
 
 Router.get('/slaanesh', (req, resp, _) => {
   resp.render('../guides/slaanesh');
 });
 Router.get('/ostermark', (req, resp, _) => {
-  resp.render('../guides/ostermark');
+  resp.render('../guides/ostermark', { palette: ostermark });
 });
 
 module.exports = Router;
