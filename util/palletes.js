2c33bb8f44c3e583e8a4d4d40a35763cec85f9ef
Matthew Masiello <sotek222@yahoo.com>
2020-04-06 01:20:40 -0400

    adds helper for color palettes

---
+++ util/palletes.js
@@ -0,0 +1,5 @@
+const ostermark = ["Chainmail Silver", "Mithril Silver", "Tin Bitz", "Dwarf Bronze", "Badab Black", "Lich Purple", "Warlock Purple", "Tausept Ochre", "Vomit Brown", "Bleached bone", "Devlin Mud", "Snakebite leather", "Enchanted Blue", "Asurman Blue", "Ice Blue", "Dark Flesh", "Dwarf Flesh", "Elf Flesh", "Skull white"];
+
+module.exports = {
+  ostermark,
+};
